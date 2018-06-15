17421d8d4f0056145f1e7cf54b9f5c39b8d99517
navog <33468084+navog@users.noreply.github.com>
2018-06-15 10:03:57 -0400

    Update Quotes.py

--- Quote of the Day/Quotes.py
+++ Quote of the Day/Quotes.py
@@ -1,5 +1,10 @@
 import random
 
-lines = open("QuoteoftheDay.txt").read().splitlines()
-quote =random.choice(lines)
+#Opens a quote filled text file and reads it line by line
+lines = open( "QuoteoftheDay.txt" ).read().splitlines()
+
+#Randomly chooses a line from the text file
+quote = random.choice( lines )
+
+#Prints the randomly chosen line
 print(quote)
