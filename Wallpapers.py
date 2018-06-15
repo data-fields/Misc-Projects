df2d22bfa00f6c02cb39dfbfb0eba082ee7c415d
navog <33468084+navog@users.noreply.github.com>
2018-06-15 10:00:22 -0400

    Update Wallpapers.py

--- Wallpapers.py
+++ Wallpapers.py
@@ -3,11 +3,11 @@ import random
 import time
 
 while True:
-    paper = open("wallpapers.txt").read().splitlines()
-    wallpaper = (random.choice(paper))
-    print(wallpaper)
+    image = open("wallpapers.txt").read().splitlines()
+    wallpaper = (random.choice(image))
     ctypes.windll.user32.SystemParametersInfoW(20,
                                            0,
                                            wallpaper,
                                            0)
     time.sleep(30)
+    
